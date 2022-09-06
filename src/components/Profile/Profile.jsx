e790a0c39b823c6a2fd6358ea67086b0ab584011
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-09-06 21:16:26 +0300

    start

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { ProfileClass, Description, Avatar } from './Profile.styled';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  followers,
+  views,
+  likes,
+}) => {
+  return (
+    <ProfileClass>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </Description>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </ProfileClass>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+};
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+export const ProfileClass = styled.div`
+  width: 400px;
+`;
+
+export const Description = styled.div`
+  padding: 20px;
+  align-items: center;
+`;
+
+export const Avatar = styled.img`
+  width: 150px;
+  border-radius: 50%;
+`;
